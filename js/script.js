0106a2755b7aca3a5d6df291626463401b349190
ragilhadi <ragilprasetyo310@gmail.com>
2020-07-31 12:40:26 +0700

    Create countdown

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var countDownDate = new Date("Aug 16, 2020 23:59:59").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("day").innerHTML = days;
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("minute").innerHTML = minutes;
+  document.getElementById("second").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+  }
+}, 1000);
